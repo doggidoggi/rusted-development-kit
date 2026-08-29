e5b95485255f5da0d68023877cbbb374ac6db012
doggidoggi <timurtihonov13@gmail.com>
2026-08-29 19:59:59 +0900

    chore: workspace formatter update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,34 @@
 import typescriptEslint from "typescript-eslint";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
-    },
+export default [
+	{
+		files: ["**/*.ts"],
+	},
+	{
+		plugins: {
+			"@typescript-eslint": typescriptEslint.plugin,
+		},
 
-    languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-    },
+		languageOptions: {
+			parser: typescriptEslint.parser,
+			ecmaVersion: 2022,
+			sourceType: "module",
+		},
 
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+		rules: {
+			"@typescript-eslint/naming-convention": [
+				"warn",
+				{
+					selector: "import",
+					format: ["camelCase", "PascalCase"],
+				},
+			],
 
-        curly: "off",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+			curly: "off",
+			eqeqeq: "warn",
+			"no-throw-literal": "warn",
+			semi: "warn",
+			indent: ["error", "tab"],
+		},
+	},
+];
